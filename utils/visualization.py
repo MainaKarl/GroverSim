d18af8b14ff780b991703eef2204bf515bf6f6cb
MainaKarl <mainakarlg@gmail.com>
2025-02-09 18:05:54 +0300

    Visualisation functionality added

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,14 +1,17 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-def measure_probabilities(state):
-    return np.abs(state)**2
-
-def plot_probabilities(probabilities, n_qubits):
-    states = [bin(i)[2:].zfill(n_qubits) for i in range(2**n_qubits)]
-    plt.bar(states, probabilities)
-    plt.xlabel("States")
+def plot_probabilities(state, title="Quantum State Probabilities"):
+    """
+    Plots the probabilities of measuring each state.
+    
+    Args:
+        state (np.ndarray): State vector.
+        title (str): Title of the plot.
+    """
+    probabilities = np.abs(state)**2
+    plt.bar(range(len(probabilities)), probabilities)
+    plt.title(title)
+    plt.xlabel("State")
     plt.ylabel("Probability")
-    plt.title("State Probabilities After Grover's Algorithm")
-    plt.xticks(rotation=90)
     plt.show()
